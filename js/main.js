de0f6b95163d5f1edbc1c65b2551bb9a8df35f7c
Papanov0 <Papanov0@yandex.ru>
2021-06-23 17:28:10 +0300

    игра угадай число

---
+++ js/main.js
@@ -0,0 +1,46 @@
+ "use strict";
+
+ let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+} 
+
+
+
+
+let guessNumber = function(){
+    const randomNumber =  Math.ceil(Math.random()*100);
+    
+    let number = prompt('Угадай число от 1 до 100');
+    
+    const getRandomNumber = function() {
+        if(Number(number) === randomNumber) {
+            alert('Поздравляю, Вы угадали!!!');
+        } else {
+            var getNumber = function() {
+                if (Number(number) > randomNumber) {
+                    number = prompt('Загаданное число меньше! Попробуй еще!');
+                } else if (Number(number) < randomNumber) {
+                    number = prompt('Загаданное число больше! Попробуй еще!');
+                }
+            return getRandomNumber();
+
+        };
+    };
+
+    return getNumber();
+    };
+
+    if (number === null) {
+        alert('Игра окончена!');
+    } else if (isNumber(number) === true) {
+        return getRandomNumber();
+    }
+    else {
+        alert('Введи число!');
+        return guessNumber();
+    }
+
+    
+};
+
+guessNumber();
